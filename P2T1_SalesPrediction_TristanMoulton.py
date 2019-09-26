e0a2ad1e6bdd0976b033207df40e55f844432fd8
moultont2123 <55841515+moultont2123@users.noreply.github.com>
2019-09-26 10:24:47 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_TristanMoulton.py
@@ -0,0 +1,14 @@
+# Write a sales prediciting program
+# 9-26-19
+# CTI-110 P2T1 - Sales Prediciton
+# Tristan Moulton
+
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
